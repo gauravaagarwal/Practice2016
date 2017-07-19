90e2d3dc439ddb3b892823cc898738de87d1de67
gauravaagarwal <gauravaagarwal@192.168.1.21>
2017-07-19 23:07:34 +0800

    scala functions with argument and return type

--- ScalaPractice/src/HelloWorld.scala
+++ ScalaPractice/src/HelloWorld.scala
@@ -1,10 +1,40 @@
 object HelloWorld {
-   /* This is my first java program.  
+  /* This is my first java program.  
    * This will print 'Hello World' as the output
    */
-   def main(args: Array[String]) {
-      println("Hello, world!") // prints Hello World
-      
-      println(1.+(2));
-   }
+  def main(args: Array[String]) {
+    println("Hello, world!") // prints Hello World
+
+    println(1.+(2));
+
+    newFunction(callThisFunction);
+     println(callThisFunctionWithReturnType);
+     callThisFunctionWithArgument(200);
+  }
+
+  def callThisFunction() {
+    println("calling function as argument");
+  }
+  def callThisFunctionWithArgument(x : Int) {
+    println("calling function with method parameter: " + x);
+  }
+    def callThisFunctionWithReturnType(): Int = {
+    println("calling function with return type and value 100");
+    100
+  }
+
+
+  def newFunction(callback: () => Unit) {
+    //while (true) {
+      callback();
+      Thread.sleep(1000);
+    //}
+  }
+  
+  def newFunctionWithReturn(callback: () => Int) {
+    while (true) {
+      println(callback());
+      Thread.sleep(1000);
+    }
+  }
 }
